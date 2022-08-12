905863e6407107f77d7ffce23c65a37de66fe3a2
Василиса <violettinsel@gmail.com>
2022-08-12 16:33:26 +0200

    Animation added

--- src/components/FinalPage.vue
+++ src/components/FinalPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class='page'>
-        <div class='result'>
+        <div class='result'  data-aos="fade-bottom" data-aos-duration="500" data-aos-easing="linear">
             <div class='final'>
                 <h1>{{ this.$store.state.userCard.title }}</h1>
                 <p>{{ this.$store.state.userCard.description }}</p>
-                <p><strong>Time to complete:</strong> {{ this.$store.state.userCard.time }} h</p>
+                <p><strong>Time to complete:</strong> {{ this.$store.state.userCard.time }}h</p>
                 <p><strong>Completed by</strong> {{ this.$store.state.userCard.doer }}</p>
                 <p><strong>Applications: </strong></p>
                 <p v-for='app in this.$store.state.userCard.apps' :key='app'>{{ app }}</p>
