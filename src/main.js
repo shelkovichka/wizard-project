f879733489dfce25eeb850d17dcd4d6f8f51375b
Василиса <violettinsel@gmail.com>
2022-08-12 16:34:30 +0200

    Library added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-console.log(store.state.userCard)
+export const app = createApp(App)
+app.AOS = new AOS.init({ disable: "phone" });
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(AOS).mount('#app')
